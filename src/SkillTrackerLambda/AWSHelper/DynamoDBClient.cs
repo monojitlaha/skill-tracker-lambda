25e06f991cfcbaf00ec004c94939ce08befd79c5
Swadhin Kumar Mukherjee <swadhinm@gmail.com>
2022-06-12 23:39:15 +0530

    DynamoDB Operations.

--- src/SkillTrackerLambda/AWSHelper/DynamoDBClient.cs
+++ src/SkillTrackerLambda/AWSHelper/DynamoDBClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using SkillTrackerLambda.Models;
 
 namespace SkillTrackerLambda.Repository
@@ -14,22 +15,26 @@ namespace SkillTrackerLambda.Repository
         }
 
         /// <summary>
-        /// Get all Profile records
-        /// </summary>
-        /// <returns></returns>
-        public async Task<List<Profile>> GetAsync()
-        {
-            return await _dynamoDbContext.ScanAsync<Profile>(new List<ScanCondition>() { }).GetRemainingAsync();
-        }
-
-        /// <summary>
         /// Get Profiles by condition
         /// </summary>
         /// <param name="condition"></param>
         /// <returns></returns>
-        public async Task<List<Profile>> GetWithScanConditionAsync(ScanCondition condition)
+        public async Task<List<Profile>> GetAsync(List<ScanCondition> condition)
         {
-            return await _dynamoDbContext.ScanAsync<Profile>(new List<ScanCondition>() { condition }).GetRemainingAsync();
+            return await _dynamoDbContext.ScanAsync<Profile>(condition).GetRemainingAsync();
+        }
+
+        public async Task<bool> SaveAsync(Profile profile)
+        {
+            await _dynamoDbContext.SaveAsync(profile);
+            return true;
+        }
+
+
+        public async Task<bool> DeleteAsync(Profile profile)
+        {
+            await _dynamoDbContext.DeleteAsync(profile);
+            return true;
         }
     }
 }
